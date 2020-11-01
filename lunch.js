410d9999f8c621ea164571eacaf8cf01d6108800
TheFatKid <tobymadsen@gmail.com>
2020-11-01 16:40:22 +0100

    Update lunch.js
    Adding the correct js

--- lunch.js
+++ lunch.js
@@ -1,37 +1,26 @@
-//This will check if today is a working day
-//A working day has to be a day between Monday or Friday
-function dayOfTheWeek () {
-	var d = new Date();
-	var n = d.getDay()
-	return n;
-}
+    function dayOfTheWeek() {
+      var d = new Date();
+      var n = d.getDay()
+      return n;
+    }
 
-function workingDay(dayOfTheWeek) {
-  return dayOfTheWeek >= 1 && dayOfTheWeek <= 5;
-}
+    function workingDay(dayOfTheWeek) {
+      return dayOfTheWeek >= 1 && dayOfTheWeek <= 5;
+    }
 
-//Refresh the functions every 5s
-/*  setTimeout(worker, 5000);*/
+    document.getElementById("workingDay").innerHTML = (workingDay(dayOfTheWeek()));
 
-console.log(workingDay(dayOfTheWeek()));
+    const lunchStart = 11 * 60 + 30;
+    const lunchEnd = 12 * 60 + 30;
+    const date = new Date();
+    const now = date.getHours() * 60 + date.getMinutes();
 
-//This will check if it is time for lunch
-//Lunch is served between 11:30 to 12:30, so we first need to check if the hour is between 11 and 12
- const lunchStart = 11 * 60 + 30;
- const lunchEnd =  12 * 60 + 30;
- const date = new Date(); 
- const now = date.getHours() * 60 + date.getMinutes();
+    function lunchTime(now) {
+      return lunchStart <= now && now <= lunchEnd
+    }
+    document.getElementById("lunchTime").innerHTML = (lunchTime(now));
 
-function lunchTime(now) {
-    return lunchStart <= now && now <= lunchEnd
-  }
-console.log(lunchTime(now));
-
-
-
-//This will check if Kim is ready with the food
-function kimIsReady () {
-//Kim is always ready
-    return true;
-}
-console.log(kimIsReady());
+    function kimIsReady() {
+      return true;
+    }
+    document.getElementById("kimIsReady").innerHTML = (kimIsReady());
